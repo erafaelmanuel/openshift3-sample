073814c84b371bb2d3ee706f5e90e94c94c237ba
erafaelmanuel <erafaelmanuel@gmail.com>
2018-03-23 16:49:42 +0800

    Create HelloController

--- src/main/java/com/example/openshift3sample/Openshift3SampleApplication.java
+++ src/main/java/com/example/openshift3sample/Openshift3SampleApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Openshift3SampleApplication {
+public class  Openshift3SampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Openshift3SampleApplication.class, args);
